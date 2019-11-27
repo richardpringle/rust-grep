e8e7059f3be2762fae81e4b2192e2ea17eb3f26c
Richard Pringle <rpring9@gmail.com>
2019-11-27 15:51:29 -0500

    Cleaner exits

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fmt::Display,
     fs::File,
     io::{self, BufReader},
 };
@@ -17,20 +18,22 @@ fn grep<R: io::BufRead>(target: &str, reader: R) -> io::Result<()> {
 fn main() {
     let mut args = std::env::args().skip(1);
 
-    let pattern = match args.next() {
-        Some(p) => p,
-        None => {
-            eprintln!("usage: grep PATTERN [FILE_1] [FILE_2]...");
-            std::process::exit(1);
-        }
-    };
+    let pattern = args.next().unwrap_or_else(|| {
+        eprintln!("usage: grep PATTERN [FILE_1] [FILE_2]...");
+        std::process::exit(1);
+    });
 
     if args.len() == 0 {
-        return grep(&pattern, io::stdin().lock()).unwrap_or_else(|e| eprintln!("{}", e));
+        return grep(&pattern, io::stdin().lock()).unwrap_or_else(print_and_exit);
     }
 
     args.for_each(|filepath| {
-        let file = File::open(filepath).expect("");
-        grep(&pattern, BufReader::new(file)).expect("");
+        let file = File::open(filepath).unwrap_or_else(print_and_exit);
+        grep(&pattern, BufReader::new(file)).unwrap_or_else(print_and_exit);
     });
+}
+
+fn print_and_exit<T, E: Display>(x: E) -> T {
+    eprintln!("{}", x);
+    std::process::exit(1);
 }
